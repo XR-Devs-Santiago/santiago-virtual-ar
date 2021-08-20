1bfa077671037b1577d6320aa8bde41c88aae124
Manny Colon <colonmanuel7@gmail.com>
2021-08-20 11:59:35 -0400

    Trabajando en seleccion de modelos3d

---
+++ Assets/Scripts/ControladorDeModelos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorDeModelos : MonoBehaviour
+{
+    
+	public void seleccionarModelo(string modeloSeleccionado) {
+		//switch(modeloSeleccionado) {
+		//case "estadio_cibao":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.estadio_cibao;
+		//	break;
+		//case "centro_leon":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.centro_leon;
+		//	break;
+		//case "monumento":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.monumento;
+		//	break;
+		//case "aurora":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.aurora;
+		//	break;
+		//case "teatro":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.teatro;
+		//	break;
+		//case "catedral":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.catedral;
+		//	break;
+		//case "fortaleza":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.fortaleza;
+		//	break;
+		//case "matum":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.matum;
+		//	break;
+		//case "camp_david":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.camp_david;
+		//	break;
+		//case "parque_central":
+		//	ValoresGlobales.modelo_seleccionado = Modelos.parque_central;
+		//	break;
+		//}
+		
+	}
+}
